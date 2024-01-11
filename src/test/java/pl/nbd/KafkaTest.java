4bba32aefd298250b01586747e4da77c7f017cbc
adam-przybylski <adamprzybylski789@gmail.com>
2024-01-11 23:53:01 +0100

    Kafka Test

---
+++ src/test/java/pl/nbd/KafkaTest.java
@@ -0,0 +1,99 @@
+package pl.nbd;
+
+import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.UUIDDeserializer;
+import org.apache.kafka.common.serialization.UUIDSerializer;
+import org.jose4j.json.internal.json_simple.JSONObject;
+import org.junit.jupiter.api.Test;
+import pl.nbd.entities.Client;
+import pl.nbd.entities.Default;
+import pl.nbd.mappers.MongoUUID;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class KafkaTest {
+
+    KafkaProducer<UUID, String> producer;
+    KafkaConsumer<UUID, String> consumer;
+
+    String topicName = "clients2";
+
+    String consumerGroupId = "clientconsumer2";
+
+    @Test
+    public void createTopic() throws InterruptedException {
+        Properties properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9192, kafka2:9292, kafka3:9392");
+        int partitionsNumber = 3;
+        short replicationFactor = 3;
+
+        try (Admin admin = Admin.create(properties)) {
+            NewTopic newTopic = new NewTopic(topicName, partitionsNumber, replicationFactor);
+            CreateTopicsOptions options = new CreateTopicsOptions()
+                    .timeoutMs(1000)
+                    .validateOnly(false)
+                    .retryOnQuotaViolation(true);
+            CreateTopicsResult result = admin.createTopics(List.of(newTopic), options);
+            KafkaFuture<Void> futureResult = result.values().get(topicName);
+            futureResult.get();
+        } catch (ExecutionException e) {
+            System.out.println(e.getCause());
+        }
+    }
+
+
+    @Test
+    public void initProducer() throws ExecutionException, InterruptedException {
+        Properties producerConfig = new Properties();
+        producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, UUIDSerializer.class.getName());
+        producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerConfig.put(ProducerConfig.CLIENT_ID_CONFIG, "local");
+        producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9192, kafka2:9292, kafka3:9392");
+        producer = new KafkaProducer<>(producerConfig);
+
+        Client client = new Default(new MongoUUID(UUID.randomUUID()), "Adam", "Przybylski", "12345678901");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", client.getId().toString());
+        jsonObject.put("firstName", client.getFirstName());
+        jsonObject.put("lastName", client.getLastName());
+        jsonObject.put("personalId", client.getPersonalId());
+        String jsonClient = jsonObject.toJSONString();
+        ProducerRecord<UUID, String> record = new ProducerRecord<>(topicName, client.getId().getUuid(), jsonClient);
+        Future<RecordMetadata> sent = producer.send(record);
+        RecordMetadata recordMetadata = sent.get();
+        System.out.println(recordMetadata);
+    }
+
+    @Test
+    public void initConsumerGroup() {
+        List<KafkaConsumer<UUID, String>> consumerGroup = new ArrayList<>();
+
+        Properties consumerConfig = new Properties();
+        consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, UUIDDeserializer.class.getName());
+        consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
+        //consumerConfig.put(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, consumerGroupId);
+        consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9192, kafka2:9292, kafka3:9392");
+
+        for (int i = 0; i < 2; i++) {
+            KafkaConsumer<UUID, String> consumer = new KafkaConsumer<>(consumerConfig);
+            consumer.subscribe(List.of(topicName));
+            consumerGroup.add(consumer);
+        }
+    }
+
+}
